202cdc61cf847d9c7f78241e87f81cc68c0b7704
jacoblurie29 <jacoblurie29@gmail.com>
2023-09-21 15:32:16 -0500

    Added page for admin volunteers

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import AdminClassesTab from "./pages/admin/AdminClassesTab/AdminClassesTab";
 import App from "./layout/App";
 import { UserProvider } from "./context/User.context";
+import AdminVolunteersTab from "./pages/admin/AdminVolunteersTab/AdminVolunteersTab";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {
@@ -22,11 +23,19 @@ const router = createBrowserRouter([
       {
         // redirect (will be conditional based on user context)
         path: "/",
-        element: <Navigate to="/admin" />,
+        element: <Navigate to="/admin/classes" />,
       },
       {
         path: "/admin",
+        element: <Navigate to="/admin/classes" />,
+      },
+      {
+        path: "/admin/classes",
         element: <AdminClassesTab />,
+      },
+      {
+        path: "/admin/volunteers",
+        element: <AdminVolunteersTab />,
       },
     ],
   },
---
+++ src/pages/admin/AdminVolunteersTab/AdminVolunteersTab.tsx
@@ -0,0 +1,11 @@
+const AdminVolunteersTab = () => {
+  console.log("AdminVolunteersTab");
+
+  return (
+    <div>
+      <h1>AdminVolunteersTab</h1>
+    </div>
+  );
+};
+
+export default AdminVolunteersTab;
