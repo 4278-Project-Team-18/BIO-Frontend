5ba00064e48e67a66eaebe34b00e14cda1af88e4
jacoblurie29 <jacoblurie29@gmail.com>
2023-09-25 11:35:36 -0500

    Lint fix in StudentLineItem

--- src/components/StudentLineItem/StudentLineItem.tsx
+++ src/components/StudentLineItem/StudentLineItem.tsx
@@ -10,54 +10,52 @@ const StudentLineItem = ({
   student,
   index,
   maxIndex,
-}: StudentLineItemProps) => {
-  return (
-    <div
-      className={`${styles["line-item-container"]} ${
-        index === 0
-          ? styles["line-item-container-first"]
-          : index === maxIndex - 1
-          ? styles["line-item-container-last"]
-          : ""
-      }`}
-    >
-      <div className={styles["line-item-container-left"]}>
-        <div className={styles["line-item-checkbox"]}>
-          {student.studentLetterLink ? (
-            <FontAwesomeIcon
-              icon={faSquareCheck}
-              className={styles["line-item-checkbox-checked"]}
-            />
-          ) : (
-            <FontAwesomeIcon
-              icon={faSquare}
-              className={styles["line-item-checkbox-unchecked"]}
-            />
-          )}
-        </div>
-        <div
-          className={styles["line-item-name"]}
-        >{`${student.firstName} ${student.lastInitial}`}</div>
-      </div>
-      <div className={styles["line-item-container-right"]}>
-        {student.studentLetterLink && (
-          <LineItemLabel
-            variant={LineItemLabelVariant.SUCCESS}
-            label={" Student Letter"}
+}: StudentLineItemProps) => (
+  <div
+    className={`${styles["line-item-container"]} ${
+      index === 0
+        ? styles["line-item-container-first"]
+        : index === maxIndex - 1
+        ? styles["line-item-container-last"]
+        : ""
+    }`}
+  >
+    <div className={styles["line-item-container-left"]}>
+      <div className={styles["line-item-checkbox"]}>
+        {student.studentLetterLink ? (
+          <FontAwesomeIcon
+            icon={faSquareCheck}
+            className={styles["line-item-checkbox-checked"]}
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faSquare}
+            className={styles["line-item-checkbox-unchecked"]}
           />
         )}
-        <button className={styles["line-item-upload-button"]}>
-          <div className={styles["line-item-upload-title"]}>
-            {student.studentLetterLink ? "Reupload" : "Upload Letter"}
-          </div>
-          <FontAwesomeIcon
-            icon={faCloudArrowUp}
-            className={styles["line-item-upload-icon"]}
-          />
-        </button>
       </div>
+      <div
+        className={styles["line-item-name"]}
+      >{`${student.firstName} ${student.lastInitial}`}</div>
     </div>
-  );
-};
+    <div className={styles["line-item-container-right"]}>
+      {student.studentLetterLink && (
+        <LineItemLabel
+          variant={LineItemLabelVariant.SUCCESS}
+          label={" Student Letter"}
+        />
+      )}
+      <button className={styles["line-item-upload-button"]}>
+        <div className={styles["line-item-upload-title"]}>
+          {student.studentLetterLink ? "Reupload" : "Upload Letter"}
+        </div>
+        <FontAwesomeIcon
+          icon={faCloudArrowUp}
+          className={styles["line-item-upload-icon"]}
+        />
+      </button>
+    </div>
+  </div>
+);
 
 export default StudentLineItem;
