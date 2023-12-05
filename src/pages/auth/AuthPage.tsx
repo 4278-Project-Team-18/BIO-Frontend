7e354cdf83be6166fb0f103a5415c73aca9632e8
jacoblurie29 <jacoblurie29@gmail.com>
2023-12-05 00:18:13 -0600

    Auth page updates

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -3,11 +3,18 @@ import styles from "./AuthPage.module.css";
 import SignUpModal from "../../modals/SignUpModal/SignUpModal";
 import SignInModal from "../../modals/SignInModal/SignInModal";
 import Logo from "../../assets/BIOLogo.png";
+import People from "../../assets/BIO-People.png";
 import type { AuthPageProps } from "./AuthPage.definitions";
 
 const AuthPage = ({ variant }: AuthPageProps) => (
   <div className={styles["auth-page-container"]}>
-    <div className={styles["auth-page-left"]}>{"You get no bitches"}</div>
+    <div className={styles["auth-page-left"]}>
+      <img src={People} alt="BIO Logo" className={styles["auth-picture"]} />
+      <div className={styles["auth-subtitle"]}>
+        Books from <span className={styles["bold"]}>us</span> to{" "}
+        <span className={styles["bold"]}>you</span>.
+      </div>
+    </div>
     <div className={styles["auth-page-right"]}>
       <div className={styles["auth-logo-container"]}>
         <img src={Logo} alt="BIO Logo" className={styles["auth-logo"]} />
